520eb4e8a7fca9cba1686a3683ed0a3fbfcceb7b
unknown <egboh.catherine@utexas.edu>
2020-02-18 21:39:06 -0600

    updated app routes

--- app.py
+++ app.py
@@ -1,46 +1,48 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import datetime
+from flask import Flask, jsonify, request
+
+# Python SQL toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Base.classes.keys()
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(bind=engine)
+
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Resources/hawaii.sqlite"
-db = SQLAlchemy(app)
-### Routes
 
-# * `/`
 
-#   * Home page.
-app.route("/")
-def hompage():
+@app.route("/")
+def home():
+    return (
+    f"<h1><center>Welcome to Surf's Up</center></br></h1>"
+    f"<center><h3>/api/v1.0/precipitation</br>"
+    f"/api/v1.0/stations</br>"
+    f"/api/v1.0/tobs</br>"
+    f"/api/v1.0/start</br>"
+    f"/api/v1.0/start/end</br></h3></center>")
 
-#   * List all routes that are available.
-app.route("/api/v1.0/precipitation")
-def precipitation()
-# * `/api/v1.0/precipitation`
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    prcp_data = (session.query(Measurement.prcp, Measurement.date).all())
+    return jsonify(prcp_data)
 
-#   * Convert the query results to a Dictionary using `date` as the key and `prcp` as the value.
 
-#   * Return the JSON representation of your dictionary.
-app.route("/api/v1.0/stations")
+@app.route("/api/v1.0/stations")
 def stations():
-# * `/api/v1.0/stations`
-
-#   * Return a JSON list of stations from the dataset.
-app.route("/api/v1.0/tobs")
-def tobs():
-# * `/api/v1.0/tobs`
-#   * query for the dates and temperature observations from a year from the last data point.
-#   * Return a JSON list of Temperature Observations (tobs) for the previous year.
-app.route("/api/v1.0/<start>")
-def summary(start):
-# * `/api/v1.0/<start>` and `/api/v1.0/<start>/<end>`
-
-app.route("/api/v1.0/<start>/<end>")
-def full_summary(start,end):
-#   * Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
-
-#   * When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date.
-
-#   * When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive.
+# List of Staions
+    station_data = (
+    session.query(Measurement.station).distinct(Measurement.station).all()
+)
+    return jsonify(station_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
