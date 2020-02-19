ffb930a57862a6b6e5292fd4d667df6d69d55b46
unknown <egboh.catherine@utexas.edu>
2020-02-18 22:00:03 -0600

    Formatted file

--- app.py
+++ app.py
@@ -23,26 +23,38 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return (
-    f"<h1><center>Welcome to Surf's Up</center></br></h1>"
-    f"<center><h3>/api/v1.0/precipitation</br>"
-    f"/api/v1.0/stations</br>"
-    f"/api/v1.0/tobs</br>"
-    f"/api/v1.0/start</br>"
-    f"/api/v1.0/start/end</br></h3></center>")
+        f"<h1><center>Welcome to Surf's Up</center></br></h1>"
+        f"<center><h3>/api/v1.0/precipitation</br>"
+        f"/api/v1.0/stations</br>"
+        f"/api/v1.0/tobs</br>"
+        f"/api/v1.0/start</br>"
+        f"/api/v1.0/start/end</br></h3></center>"
+    )
+
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    prcp_data = (session.query(Measurement.prcp, Measurement.date).all())
+    prcp_data = session.query(Measurement.prcp, Measurement.date).all()
     return jsonify(prcp_data)
 
 
 @app.route("/api/v1.0/stations")
 def stations():
-# List of Staions
+    # List of Stations
     station_data = (
-    session.query(Measurement.station).distinct(Measurement.station).all()
-)
+        session.query(Measurement.station).distinct(Measurement.station).all()
+    )
     return jsonify(station_data)
+
+
+# @app.route("/api/v1.0/tobs")
+# def tobs():
+
+# @app.route("/api/v1.0/<start>")
+# def startdate(start):
+
+# @app.route("/api/v1.0/<start>/<end>")
+# def fulldate(start,end):
 
 if __name__ == "__main__":
     app.run(debug=True)
